4cde8d0f88b3abcff938b7d44762e549344ecfaa
Bhaveshreddy5 <149860146+Bhaveshreddy5@users.noreply.github.com>
2024-09-28 06:59:40 +0530

    Add files via upload

---
+++ static/js/products.js
@@ -0,0 +1,76 @@
+const products = [
+    { 
+        id: 'n8', 
+        brand: 'ZARA', 
+        name: 'Slim Fit Half-Sleeve Shirt', 
+        price: 'Rs.1499', 
+        image: '/static/images/products/n8.jpg',  
+        rating: 4,
+        description: "Elevate your casual look with this slim-fit half-sleeve shirt from ZARA. Designed for ultimate comfort and a tailored fit, this shirt is perfect for any occasion. Pair it with jeans or chinos for a stylish, everyday outfit."
+    },
+    { 
+        id: 'n7', 
+        brand: 'ZARA', 
+        name: 'Brown Full-Sleeve Shirt', 
+        price: 'Rs.2499', 
+        image: '/static/images/products/n7.jpg',  
+        rating: 5,
+        description: "Crafted with premium fabric, this brown full-sleeve shirt from ZARA is both stylish and versatile. The deep, rich color adds sophistication to your wardrobe, making it ideal for formal and casual wear alike."
+    },
+    { 
+        id: 'n5', 
+        brand: 'ZARA', 
+        name: 'Slim Fit Blue Shirt', 
+        price: 'Rs.1999', 
+        image: '/static/images/products/n5.jpg', 
+        rating: 5,
+        description: "Get the perfect combination of style and comfort with this slim-fit blue shirt from ZARA. Its lightweight fabric and modern fit make it a go-to piece for any casual outing or business meeting."
+    },
+    { 
+        id: 'n4', 
+        brand: 'ZARA', 
+        name: 'Printed Green Shirt', 
+        price: 'Rs.999', 
+        image: '/static/images/products/n4.jpg', 
+        rating: 3,
+        description: "Make a bold statement with this printed green shirt from ZARA. The unique pattern and fresh color bring a touch of individuality to your outfit, making it a standout choice for any occasion."
+    },
+    { 
+        id: 'n3', 
+        brand: 'H & M', 
+        name: 'White Slim Fit Easy-Iron Shirt', 
+        price: 'Rs.1499', 
+        image: '/static/images/products/n3.jpg', 
+        rating: 4,
+        description: "Sleek and simple, this white slim-fit easy-iron shirt from H & M is a wardrobe essential. Made from soft cotton, it ensures all-day comfort, while the wrinkle-free fabric makes it easy to maintain."
+    },
+    { 
+        id: 'n2', 
+        brand: 'H & M', 
+        name: 'Silver Full-Sleeve Shirt', 
+        price: 'Rs.1899', 
+        image: '/static/images/products/n2.jpg', 
+        rating: 5,
+        description: "Shine bright in this silver full-sleeve shirt from H & M. With its eye-catching color and smooth finish, it’s perfect for special occasions or when you want to stand out in a crowd."
+    },
+    { 
+        id: 'n1', 
+        brand: 'H & M', 
+        name: 'Blue Slim Fit Easy-Iron Shirt', 
+        price: 'Rs.1499', 
+        image: '/static/images/products/n1.jpg', 
+        rating: 4,
+        description: "Stay sharp and wrinkle-free all day with this blue slim-fit easy-iron shirt from H & M. Designed for the modern professional, its breathable fabric and sleek cut offer both style and practicality, making it a perfect choice for any occasion."
+    },
+    { 
+        id: 'f6', 
+        brand: 'H & M', 
+        name: 'Multi-Colour Full-Sleeve Shirt', 
+        price: 'Rs.2399', 
+        image: '/static/images/products/f6.jpg',  
+        rating: 3,
+        description: "Add some fun to your wardrobe with this multi-color full-hands shirt from H & M. Featuring a vibrant mix of colors, it's a perfect choice for those who like to express their personality through fashion."
+    }
+];
+
+export { products };
